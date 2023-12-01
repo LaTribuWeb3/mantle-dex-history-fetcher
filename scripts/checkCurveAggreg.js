ed5cc2fc2f674196acc8ce55de988f30506ba917
Alfu <Alfulinku@gmail.com>
2023-12-01 11:57:37 +0100

    Update checkCurveAggreg.js

--- scripts/checkCurveAggreg.js
+++ scripts/checkCurveAggreg.js
@@ -1,29 +1,24 @@
 const { getLiquidity } = require('../src/data.interface/data.interface');
+const fs = require('fs');
 
 async function curveAggreg() {
 
-    const base = 'wstETH';
-    const quote = 'WETH';
-    const liquidity_nojumps = getLiquidity('curve',base, quote, 18_000_000, 18_200_000, false);
-    if(liquidity_nojumps) {
-        console.log(Object.values(liquidity_nojumps)[0].slippageMap[500]);
-    } else {
-        console.log('no liquidity without jump');
-    }
-    
-    const liquidity_withjumps = getLiquidity('curve',base, quote, 18_000_000, 18_200_000, true);
-    if(liquidity_withjumps) {
-        console.log(Object.values(liquidity_withjumps)[0].slippageMap[500]);
-    } else {
-        console.log('no liquidity with jump');
-    }
+    const bases = ['wstETH', 'stETH', 'USDC', 'LDO', 'CVX', 'CRV', 'WBTC', 'WETH'];
+    const quotes = ['WETH', 'USDC', 'WBTC'];
+    fs.writeFileSync('liquidity_with_jumps.csv', 'base,quote,liquidity 5%\n' );
+    for(const base of bases) {
+        for(const quote of quotes) {
+            if(base == quote) continue;
 
-    
-    const liquidityuniv3 = getLiquidity('uniswapv3',base, quote, 18_000_000, 18_200_000, true);
-    if(liquidityuniv3) {
-        console.log(Object.values(liquidityuniv3)[0].slippageMap[500]);
-    } else {
-        console.log('no liquidity univ3 with jump');
+            const liquidity_withjumps = getLiquidity('curve',base, quote, 18_000_000, 18_200_000, true);
+            if(liquidity_withjumps) {
+                const liquidity = Object.values(liquidity_withjumps)[0].slippageMap[500];
+                console.log(liquidity);
+                fs.appendFileSync('liquidity_with_jumps.csv', `${base},${quote},${liquidity.base}\n` );
+            } else {
+                console.log(`no liquidity for ${base}/${quote}`);
+            }
+        }
     }
 }
 
