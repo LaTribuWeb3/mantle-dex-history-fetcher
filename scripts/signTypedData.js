b231baeec3d40b2adcb26af0a429d11450b26cdf
Alfu <Alfulinku@gmail.com>
2023-12-11 12:07:10 +0100

    Update signTypedData.js

--- scripts/signTypedData.js
+++ scripts/signTypedData.js
@@ -8,8 +8,6 @@ const { BN_1e18 } = require('../src/utils/constants');
 const SPythiaAbi = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"inputs":[],"name":"DOMAIN_SEPARATOR","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"EIP712DOMAIN_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"RISKDATA_TYPEHASH","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"chainId","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"address","name":"collateralAsset","type":"address"},{"internalType":"address","name":"debtAsset","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"volatility","type":"uint256"},{"internalType":"uint256","name":"lastUpdate","type":"uint256"},{"internalType":"uint256","name":"chainId","type":"uint256"}],"internalType":"struct SPythia.RiskData","name":"data","type":"tuple"},{"internalType":"uint8","name":"v","type":"uint8"},{"internalType":"bytes32","name":"r","type":"bytes32"},{"internalType":"bytes32","name":"s","type":"bytes32"}],"name":"getSigner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"components":[{"internalType":"address","name":"collateralAsset","type":"address"},{"internalType":"address","name":"debtAsset","type":"address"},{"internalType":"uint256","name":"liquidity","type":"uint256"},{"internalType":"uint256","name":"volatility","type":"uint256"},{"internalType":"uint256","name":"lastUpdate","type":"uint256"},{"internalType":"uint256","name":"chainId","type":"uint256"}],"internalType":"struct SPythia.RiskData","name":"data","type":"tuple"}],"name":"hashStruct","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"},{"inputs":[{"components":[{"internalType":"string","name":"name","type":"string"},{"internalType":"string","name":"version","type":"string"},{"internalType":"uint256","name":"chainId","type":"uint256"},{"internalType":"address","name":"verifyingContract","type":"address"}],"internalType":"struct SPythia.EIP712Domain","name":"eip712Domain","type":"tuple"}],"name":"hashStruct","outputs":[{"internalType":"bytes32","name":"","type":"bytes32"}],"stateMutability":"pure","type":"function"}]
 
 async function signTypedData() {
-
-    
     const web3Provider = new ethers.providers.StaticJsonRpcProvider('https://eth.llamarpc.com');
     const base = getConfTokenBySymbol('wstETH');
     const quote =  getConfTokenBySymbol('WETH');
@@ -26,6 +24,11 @@ async function signTypedData() {
     const signature = await wallet._signTypedData(typedData.domain, typedData.types, typedData.value);
     const splitSig = ethers.utils.splitSignature(signature);
     console.log(splitSig);
+    /*
+        v: 28,
+        r: '0xbafb174e0605f88711e19eb6b0c8aff8e18cc503fd23dc2116c1e1c075369348',
+        s: '0x55915af7d1442e85d1c5551027d5180af1058c36e0ca112a94093e72459fb9cb',
+  */
     // const sigBytes =  joinSignature(splitSig)
     // const splitSig = ethers.utils.Signature.from(signature);
 
@@ -43,6 +46,29 @@ async function signTypedData() {
     console.log(signer);
 
     if(signer != wallet.address) {
+        throw new Error('SIGNER IS NOT WALLET PUBLIC KEY');
+    } else {
+        console.log('Signer is our wallet !');
+    }
+
+    const fakeValues = {
+        collateralAsset: base.address,
+        debtAsset: quote.address,
+        liquidity: '10000000000000000000000000000000000000',
+        volatility: '0',
+        lastUpdate: Math.round(Date.now()/1000),
+        chainId: 5
+    };
+    const signer_fakedata = await spythia.getSigner(
+        fakeValues,
+        splitSig.v,
+        splitSig.r,
+        splitSig.s
+    );
+
+    console.log(signer_fakedata);
+
+    if(signer_fakedata != wallet.address) {
         throw new Error('SIGNER IS NOT WALLET PUBLIC KEY');
     } else {
         console.log('Signer is our wallet !');
