69258d7d8578d5b041b433c4dbd07acf178749b8
Alfu <Alfulinku@gmail.com>
2024-01-17 17:36:46 +0100

    Update checkLiquidity.js

--- scripts/checkLiquidity.js
+++ scripts/checkLiquidity.js
@@ -1,7 +1,18 @@
-const { getLiquidity } = require("../src/data.interface/data.interface");
+const { getLiquidity } = require('../src/data.interface/data.interface');
 
 async function checkLiquidity() {
-    const liquidity = getLiquidity('curve', 'LDO', 'WETH', 18_000_000, 19_000_000, true);
+    let liquidity = getLiquidity('uniswapv3', 'wstETH', 'USDC', 19_000_000, 19_018_931, true);
+    console.log('uniswapv3', liquidity[19018900].slippageMap[500].base);
+    liquidity = getLiquidity('curve', 'wstETH', 'USDC', 19_000_000, 19_018_931, true);
+    console.log('curve', liquidity[19018900].slippageMap[500].base);
 }
 
+
+/*
+    wstETH->USDC => RIEN
+    via pivot WETH => wstETH->WETH->USDC (avant => wstETH->WETH n'existe pas sur curve)
+    via pivot USDC NON CAR QUOTE = USDC, sinon ça fera wstETH->USDC->USDC
+    via pivot WBTC => wstETH->WBTC->USDC mais wstETH->WBTC n'existe pas sur curve
+
+*/
 checkLiquidity();
