6e86726d2a814d6771d8fa9073ef3b0ca4311ab0
Alfu <Alfulinku@gmail.com>
2024-04-30 13:05:46 +0200

    updt checks

--- scripts/checkLiquidity.js
+++ scripts/checkLiquidity.js
@@ -1,14 +1,15 @@
-const { getLiquidity, getLiquidityAll, getLiquidityV2, getLiquidityAverageV2 } = require('../src/data.interface/data.interface');
+const { getLiquidityV2, getLiquidityAverageV2, getRollingVolatility } = require('../src/data.interface/data.interface');
 const { watchedPairs } = require('../src/global.config');
-const { PLATFORMS, BLOCK_PER_DAY } = require('../src/utils/constants');
-const fs = require('fs');
+const { BLOCK_PER_DAY } = require('../src/utils/constants');
 const { roundTo } = require('../src/utils/utils');
 const { getPriceAtBlock } = require('../src/data.interface/internal/data.interface.price');
+const { ethers } = require('ethers');
 
 async function checkLiquidity() {
+    const web3Provider = new ethers.providers.StaticJsonRpcProvider('https://eth.llamarpc.com');
 
-    const block = 19673524;
-    const base = 'pufETH';
+    const block = await web3Provider.getBlockNumber();
+    const base = 'ezETH';
     const quote = 'WETH';
     const platform = 'all';
 
@@ -27,6 +28,10 @@ async function checkLiquidity() {
     }
 
     console.log(`platform ${platform} ${base} ${quote} avg 30d : ${valueFor5PctAvg}`);
+
+    const volatility = await getRollingVolatility(platform, base, quote, web3Provider);
+    console.log(volatility.latest);
+    
 }
 
 checkLiquidity();
--- scripts/checkVolatility.js
+++ scripts/checkVolatility.js
@@ -1,47 +1,21 @@
-const { getPricesAtBlockForIntervalViaPivot, getPricesAtBlockForIntervalViaPivots } = require('../src/data.interface/internal/data.interface.utils');
-const { medianPricesOverBlocks, computeBiggestDailyChange, rollingBiggestDailyChange } = require('../src/utils/volatility');
+const { getLiquidityV2, getLiquidityAverageV2, getRollingVolatility } = require('../src/data.interface/data.interface');
+const { watchedPairs } = require('../src/global.config');
+const { BLOCK_PER_DAY } = require('../src/utils/constants');
+const { roundTo } = require('../src/utils/utils');
+const { getPriceAtBlock } = require('../src/data.interface/internal/data.interface.price');
 const { ethers } = require('ethers');
 const fs = require('fs');
-const path = require('path');
-const { DATA_DIR, HALF_LIFE_1Y, HALF_LIFE_2Y } = require('../src/utils/constants');
-const { getRollingVolatility } = require('../src/data.interface/data.interface');
 
 async function checkVolatility() {
-
-    const base = 'WETH';
-    const quote = 'wstETH';
-    const prices = getPricesAtBlockForIntervalViaPivots('uniswapv3', base, quote, 0, 19_000_000);
-    const medianed = medianPricesOverBlocks(prices, undefined);
-
     const web3Provider = new ethers.providers.StaticJsonRpcProvider('https://eth.llamarpc.com');
-    let volatility_all = await rollingBiggestDailyChange(medianed, web3Provider);
-    console.log(volatility_all.latest.current);
-    volatility_all = await getRollingVolatility('all', base, quote, web3Provider);
-    console.log(volatility_all.latest.current);
+    const base = 'ezETH';
+    const quote = 'WETH';
+    const platform = 'all';
 
-    // fs.writeFileSync('volatility.json', JSON.stringify(volatility_all, null, 2));
-
-    // const volatility_all = await getRollingVolatility('all', base, quote, web3Provider);
-    // fs.writeFileSync('volatility.json', JSON.stringify(volatility_all, null, 2));
-
-    // const volatility_1y = await getRollingVolatility('uniswapv3', 'wstETH', 'WETH', web3Provider);
-    // fs.writeFileSync('volatility.json', JSON.stringify(volatility_1y, null, 2));
-    // fs.writeFileSync('volatility.csv', 'blockstart,blockend,yesterday,current,minprice,maxprice\n');
+    const volatility = await getRollingVolatility(platform, base, quote, web3Provider);
+    console.log(`${platform} ${base} ${quote} latest voaltility: `, volatility.latest);
+    fs.writeFileSync('volatility.json', JSON.stringify(volatility, null, 2));
     
-    // for(const vol of volatility_1y.history) {
-    //     fs.appendFileSync('volatility.csv', `${vol.blockStart},${vol.blockEnd},${vol.yesterday},${vol.current},${vol.minPrice},${vol.maxPrice}\n`);
-    // }
-
-    // const volatility_2y = await getRollingVolatility('uniswapv3', 'wstETH', 'WETH', web3Provider, HALF_LIFE_2Y);
-    // fs.writeFileSync('volatility_2y.json', JSON.stringify(volatility_2y, null, 2));
-
-
-    // fs.writeFileSync('volatility_2y.csv', 'blockstart,blockend,yesterday,current,minprice,maxprice\n');
-
-    // for(const vol of volatility_2y.history) {
-    //     fs.appendFileSync('volatility_2y.csv', `${vol.blockStart},${vol.blockEnd},${vol.yesterday},${vol.current},${vol.minPrice},${vol.maxPrice}\n`);
-    // }
-
 }
 
 checkVolatility();
